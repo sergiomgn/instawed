791193a27f717fcd6732ce409a7068619a76f11a
Sergio Neves <sergiomgn20@gmail.com>
2024-06-02 14:14:54 +0100

    Adding DB and JWT token authentication

--- models/models.go
+++ models/models.go
@@ -1,19 +1,41 @@
 package models
 
 import (
-	"log"
-
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+type Photo struct {
+	gorm.Model
+	UserID uint
+	URL    string
+}
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"unique"`
+	Email    string
+	Photos   []Photo
+}
+
 func InitDB() {
 	var err error
 	DB, err = gorm.Open(sqlite.Open("InstaWed.db"), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to Database")
-		log.Fatal("Failed to connec to Database")
 	}
+
+	DB.AutoMigrate(&User{}, &Photo{})
+}
+
+func FindUserByUsername(username string) (*User, error) {
+	var user User
+	result := DB.Where("username = ?", username).First(&user)
+	return &user, result.Error
+}
+
+func CreateUser(user *User) error {
+	return DB.Create(user).Error
 }
